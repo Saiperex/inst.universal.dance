59d42c34b409a24d311991acd2c4b68bc942a983
Xavier <xaviramos668@gmail.com>
2022-11-07 16:45:44 -0300

    resolviendo el responsive

--- scripts/script-menu.js
+++ scripts/script-menu.js
@@ -1,7 +1,26 @@
 const btnmenu=document.getElementById("btn-menu");
 var estado=0;
-document.getElementById("contenedor-menu").style.width="4%";
+resolucion();
 document.getElementById("texto").style.display="none";
+var label= document.querySelectorAll("label");
+for(let i=0;i<label.length;i++)
+{
+    label[i].style.display="none";
+}
+
+function resolucion()
+{  
+    var ancho=window.screen.width;
+    if(ancho>400)
+    {
+        document.getElementById("contenedor-menu").style.width="4%";
+    }
+    else
+    {
+        document.getElementById("contenedor-menu").style.width="14%";
+    }
+}
+
 function estadobtn()
 {
     if(estado==0)
@@ -15,14 +34,29 @@ function estadobtn()
 }
 function menu ()
 {
-    if(estado==0)
+    if(window.screen.width>400)
     {
-        document.getElementById("contenedor-menu").style.width="4%";
+        if(estado==0)
+        {
+            document.getElementById("contenedor-menu").style.width="4%";
+        }
+        else
+        {
+            document.getElementById("contenedor-menu").style.width="14%";
+        
+        }
     }
     else
     {
-        document.getElementById("contenedor-menu").style.width="14%";
-  
+        if(estado==0)
+        {
+            document.getElementById("contenedor-menu").style.width="14%";
+        }
+        else
+        {
+            document.getElementById("contenedor-menu").style.width="44%";
+        
+        }
     }
     document.getElementById("contenedor-menu").style.transition="all 500ms"
 }
@@ -42,7 +76,6 @@ function letrasAparecen()
 }
 function listaOcultar()
 {
-   var label= document.querySelectorAll("label");
    if(estado==0)
    {
         for(let i=0;i<label.length;i++)
@@ -67,4 +100,3 @@ function escondermostrarMenu(evento)
     setTimeout(letrasAparecen,500);
 }
 btnmenu.onclick=escondermostrarMenu;
-listaOcultar();
